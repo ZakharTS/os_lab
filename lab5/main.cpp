ddfeec982213b33be01e00de1b37c63beb2cb067
Zakhar <zaharsehar@gmail.com>
2022-11-29 17:43:32 +0300

    Update lab5/main.cpp

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -18,60 +18,63 @@ int main() {
     char curChar = ' ';
     int strNumber = 0;
     size_t sizeRead = 1;
-    while (sizeRead) {
-        strNumber++;
-        sizeRead = read(0, &curChar, 1);
-        if (!sizeRead) return -1;
-
-        if (curChar != 'a' && curChar != 'A' && curChar != 'e' && curChar != 'E' && curChar != 'i' && curChar != 'I'
-        && curChar != 'o' && curChar != 'O' && curChar != 'u' && curChar != 'U' && curChar != 'y' && curChar != 'Y') {
-            for (; curChar != '\n';) {
-                sizeRead = read(0, &curChar, 1);
-            }
-            continue;
-        }
-        char str[256];
-        str[0] = curChar;
-        int i = 1;
-        for (; curChar != '\n'; i++) {
-            sizeRead = read(0, &curChar, 1);
-            str[i] = curChar;
-        }
-        write(1, str, i);
-        char strNumChar[256];
-        int strNumCharSize = sprintf(strNumChar, "%d\n", strNumber);
-        write(2, strNumChar, strNumCharSize);
-    }
-
-    // part 2
-
 //    while (sizeRead) {
 //        strNumber++;
 //        sizeRead = read(0, &curChar, 1);
 //        if (!sizeRead) return -1;
 //
-//        if (((curChar >= 64 && curChar <= 90) || (curChar >= 97 && curChar <= )) && (curChar != 'a' && curChar != 'A' && curChar != 'e' && curChar != 'E' && curChar != 'i' && curChar != 'I'
-//            && curChar != 'o' && curChar != 'O' && curChar != 'u' && curChar != 'U' && curChar != 'y' && curChar != 'Y')) {
-//            char str[256];
+//        if (curChar != 'a' && curChar != 'A' && curChar != 'e' && curChar != 'E' && curChar != 'i' && curChar != 'I'
+//            && curChar != 'o' && curChar != 'O' && curChar != 'u' && curChar != 'U' && curChar != 'y' &&
+//            curChar != 'Y') {
+//            for (; curChar != '\n';) {
+//                sizeRead = read(0, &curChar, 1);
+//            }
+//            continue;
+//        }
+//        char str[256];
 //        str[0] = curChar;
 //        int i = 1;
 //        for (; curChar != '\n'; i++) {
 //            sizeRead = read(0, &curChar, 1);
 //            str[i] = curChar;
 //        }
-//        if (strNumber % 2 == 1) {
-//            write(1, str, i);
-//        }
-//        if (strNumber % 2 == 0) {
-//            write(2, str, i);
-//        }
-//        } else {
-//	for (; curChar != '\n';) {
-//                sizeRead = read(0, &curChar, 1);
-//            }
-//		  }
-//        
+//        write(1, str, i);
+//        char strNumChar[256];
+//        int strNumCharSize = sprintf(strNumChar, "%d\n", strNumber);
+//        write(2, strNumChar, strNumCharSize);
 //    }
+
+    // part 2
+    int f1 = open("file1", O_WRONLY | O_CREAT, 0666), f2 = open("file2", O_WRONLY | O_CREAT, 0666);
+
+    while (sizeRead) {
+        strNumber++;
+        sizeRead = read(0, &curChar, 1);
+        if (!sizeRead) return -1;
+
+        if (((curChar >= 64 && curChar <= 90) || (curChar >= 97 && curChar <= 122)) &&
+            (curChar != 'a' && curChar != 'A' && curChar != 'e' && curChar != 'E' && curChar != 'i' && curChar != 'I'
+             && curChar != 'o' && curChar != 'O' && curChar != 'u' && curChar != 'U' && curChar != 'y' &&
+             curChar != 'Y')) {
+            char str[256];
+            str[0] = curChar;
+            int i = 1;
+            for (; curChar != '\n'; i++) {
+                sizeRead = read(0, &curChar, 1);
+                str[i] = curChar;
+            }
+            if (strNumber % 2 == 1) {
+                write(f1, str, i);
+            }
+            if (strNumber % 2 == 0) {
+                write(f2, str, i);
+            }
+        } else {
+            for (; curChar != '\n';) {
+                sizeRead = read(0, &curChar, 1);
+            }
+        }
+    }
 
     return 0;
 }
